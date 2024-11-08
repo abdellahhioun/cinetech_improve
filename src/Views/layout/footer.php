f1ab2ee11b63dbe8c7faf370b8f38b84bbcf7a45
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-08 12:02:29 +0100

    Create footer.php

---
+++ src/Views/layout/footer.php
@@ -0,0 +1,9 @@
+    </div>
+    <footer class="bg-dark text-light mt-5 py-3">
+        <div class="container">
+            <p class="text-center mb-0">© 2024 Cinetech Improve. Powered by TMDB API.</p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
