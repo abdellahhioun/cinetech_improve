572663607f0682ca14ad9e3b7af7fda19c3a18b4
abd-ellah hioun <abd-ellah.hioun@laplateforme.io>
2024-11-14 15:38:08 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,3 +2,9 @@
 // config/config.php
 define('TMDB_API_KEY', '11e5f3e682a7ac70b045778a154ba030');
 define('TMDB_API_BASE_URL', 'https://api.themoviedb.org/3');
+
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cinetech_improve');
+define('DB_USER', 'root');
+define('DB_PASS', '');
